5a265740c82c74e1f96b2583c8aeeb4196110ad4
kodi73 <aditya.kumar.73@outlook.com>
2026-07-21 19:29:04 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.print("$ ");
+        Scanner sc = new Scanner(System.in);
+        String command = sc.next();
+        System.out.println(command + " command not found");
     }
 }
