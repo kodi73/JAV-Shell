a137e5fe4d1dd152e30734f5b720f8e96e72527b
kodi73 <aditya.kumar.73@outlook.com>
2026-07-23 00:57:04 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,7 +15,7 @@ public class Main {
             } else if (command.startsWith("echo")) {
                 System.out.println(command.substring(5));
             } else if (command.startsWith("type")) {
-                if (arguments.equals("echo") || arguments.equals("exit")) {
+                if (arguments.equals("echo") || arguments.equals("exit") || arguments.equals("type")) {
                     System.out.println(arguments + " is a shell builtin");
                 }
             } else {
