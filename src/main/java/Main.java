5f6848962a4a915ae4982d4249fa46028ab704a8
kodi73 <aditya.kumar.73@outlook.com>
2026-07-23 01:07:19 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,27 +3,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        
         while (true) {
             System.out.print("$ ");
             String input = sc.nextLine();
-            String command = input.substring(0,4).trim();
-            String arguments = input.substring(5).trim();
-            
-            if (command.trim().equals("exit")) {
+
+            int spaceIndex = input.indexOf(' ');
+            String command = spaceIndex == -1 ? input : input.substring(0, spaceIndex);
+            String arguments = spaceIndex == -1 ? "" : input.substring(spaceIndex + 1).trim();
+
+            if (command.equals("exit")) {
                 break;
-            } else if (command.startsWith("echo")) {
+            } else if (command.equals("echo")) {
                 System.out.println(arguments);
-            } else if (command.startsWith("type")) {
+            } else if (command.equals("type")) {
                 if (arguments.equals("echo") || arguments.equals("exit") || arguments.equals("type")) {
                     System.out.println(arguments + " is a shell builtin");
                 } else {
-                System.out.println(arguments + ": not found");
+                    System.out.println(arguments + ": not found");
                 }
             } else {
                 System.out.println(command + ": command not found");
             }
         }
-        sc.close();     
+        sc.close();
     }
 }
